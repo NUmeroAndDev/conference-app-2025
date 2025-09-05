d5a244c9c08511cae362b8a84407dde826bdba8e
geckour <geckour@gmail.com>
2025-09-06 04:45:55 +0900

    Fix to prevent hiding content by FAB on TimetableItemDetailScreen

--- feature/sessions/src/commonMain/kotlin/io/github/droidkaigi/confsched/sessions/TimetableItemDetailScreen.kt
+++ feature/sessions/src/commonMain/kotlin/io/github/droidkaigi/confsched/sessions/TimetableItemDetailScreen.kt
@@ -1,16 +1,22 @@
 package io.github.droidkaigi.confsched.sessions
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeGestures
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 import io.github.droidkaigi.confsched.designsystem.theme.ProvideRoomTheme
@@ -46,6 +52,7 @@ fun TimetableItemDetailScreen(
 ) {
     ProvideRoomTheme(uiState.timetableItem.room.roomTheme) {
         val listState = rememberLazyListState()
+        var fabHeight by remember { mutableStateOf(0.dp) }
         Scaffold(
             topBar = {
                 TimetableItemDetailTopAppBar(
@@ -53,6 +60,7 @@ fun TimetableItemDetailScreen(
                 )
             },
             floatingActionButton = {
+                val density = LocalDensity.current
                 TimetableItemDetailFloatingActionButtonMenu(
                     isBookmarked = uiState.isBookmarked,
                     slideUrl = uiState.timetableItem.asset.slideUrl,
@@ -62,7 +70,11 @@ fun TimetableItemDetailScreen(
                     onShareClick = { onShareClick(uiState.timetableItem) },
                     onViewSlideClick = onLinkClick,
                     onWatchVideoClick = onLinkClick,
-                    modifier = Modifier.padding(bottom = WindowInsets.safeGestures.asPaddingValues().calculateBottomPadding()),
+                    modifier = Modifier.onGloballyPositioned {
+                        with(density) {
+                            fabHeight = it.size.height.toDp()
+                        }
+                    }.padding(bottom = WindowInsets.safeGestures.asPaddingValues().calculateBottomPadding()),
                 )
             },
             contentWindowInsets = WindowInsets(),
@@ -74,7 +86,7 @@ fun TimetableItemDetailScreen(
                     .fillMaxSize()
                     .enableMouseDragScroll(listState)
                     .testTag(TimetableItemDetailScreenLazyColumnTestTag),
-                contentPadding = contentPadding + WindowInsets.navigationBars.asPaddingValues(),
+                contentPadding = contentPadding + PaddingValues(bottom = WindowInsets.safeGestures.asPaddingValues().calculateBottomPadding() + fabHeight),
             ) {
                 item {
                     TimetableItemDetailHeadline(
