9ddc08f5b0fa23dc0457d82b1394060d4034b2a9
todayama_r <13657682+Corvus400@users.noreply.github.com>
2025-09-09 02:11:38 +0900

    :wrench: I modified the implementation so that the JVM build passes.

--- core/droidkaigiui/src/commonMain/kotlin/io/github/droidkaigi/confsched/droidkaigiui/WithDeviceOrientation.kt
+++ core/droidkaigiui/src/commonMain/kotlin/io/github/droidkaigi/confsched/droidkaigiui/WithDeviceOrientation.kt
@@ -1,21 +1,11 @@
 package io.github.droidkaigi.confsched.droidkaigiui
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.compose.LocalLifecycleOwner
 
-interface DeviceOrientationScope {
-    val orientation: Orientation
-}
-
-private class DeviceOrientationScopeImpl : DeviceOrientationScope {
+internal class DeviceOrientationScopeImpl : DeviceOrientationScope {
     override var orientation: Orientation by mutableStateOf(Orientation.Zero)
         private set
 
@@ -24,33 +14,11 @@ private class DeviceOrientationScopeImpl : DeviceOrientationScope {
     }
 }
 
-@Composable
-fun WithDeviceOrientation(
-    content: @Composable (DeviceOrientationScope.() -> Unit),
-) {
-    val lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current
-
-    val scope = remember {
-        DeviceOrientationScopeImpl()
-    }
-    val sensorManager = getOrientationSensorManager {
-        scope.updateOrientation(it)
-    }
-
-    DisposableEffect(lifecycleOwner) {
-        val observer = LifecycleEventObserver { _, event ->
-            if (event == Lifecycle.Event.ON_RESUME) {
-                sensorManager.start()
-            } else if (event == Lifecycle.Event.ON_PAUSE) {
-                sensorManager.stop()
-            }
-        }
-
-        lifecycleOwner.lifecycle.addObserver(observer)
-        onDispose {
-            lifecycleOwner.lifecycle.removeObserver(observer)
-        }
-    }
-
-    content(scope)
+interface DeviceOrientationScope {
+    val orientation: Orientation
 }
+
+@Composable
+expect fun WithDeviceOrientation(
+    content: @Composable (DeviceOrientationScope.() -> Unit),
+)
